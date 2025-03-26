9cd98a93a487fb0075e26086c94d457228485343
julessaladzius <jules.saladzius@gmail.com>
2025-03-26 19:09:36 -0400

    INVERSÉ LONGITUDE. LATITUDE DOIT ÊTRE ENVIRON 45.

--- Scripts/fct_cleanup_geom.R
+++ Scripts/fct_cleanup_geom.R
@@ -9,17 +9,30 @@ library("tidyverse") #nécessite l'installation du package 'tidyverse'
 # 4) Vérification et correction de la longitude
 # 5) Retour du dataframe nettoyé
 
+
 cleanup_geom <- function(dataframe){
-	fixed.dataframe=NULL
-	fixed.dataframe <- tidyr::extract(dataframe,geom,c("latitude","longitude"),"\\(\\((.*)\\s(.*)\\)\\)") #sépare la latitude et la longitude en deux colonnes distinctes
-	fixed.dataframe[,c("latitude","longitude")] <- sapply(fixed.dataframe[,c("latitude","longitude")],as.numeric) #converti en valeurs numériques
-	verif.latitude <- function(x,max=90,min=-90){
-		replace(x,x>max|x<min,NA)
-	}
-	fixed.dataframe[,"latitude"] <- verif.latitude(fixed.dataframe[,"latitude"]) #fonction qui vérifie si les valeurs de latitude sont valides (+-90), sinon remplace par un NA
-	verif.longitude <- function(x,max=180,min=-180){
-		replace(x,x>max|x<min,NA)
-	}
-	fixed.dataframe[,"longitude"] <- verif.longitude(fixed.dataframe[,"longitude"]) #fonction qui vérifie si les valeurs de longitude sont valides (+-180), sinon remplace par un NA
-	fixed.dataframe
+  fixed.dataframe = NULL
+  fixed.dataframe <- tidyr::extract(
+    dataframe,
+    geom,
+    c("longitude", "latitude"), # inverse ici : la première valeur est la longitude, la deuxième est la latitude
+    "\\(\\((.*)\\s(.*)\\)\\)"
+  ) # sépare la longitude et la latitude en deux colonnes distinctes
+  
+  fixed.dataframe[, c("latitude", "longitude")] <- sapply(
+    fixed.dataframe[, c("latitude", "longitude")],
+    as.numeric
+  ) # converti en valeurs numériques
+  
+  verif.latitude <- function(x, max = 90, min = -90){
+    replace(x, x > max | x < min, NA)
+  }
+  fixed.dataframe[,"latitude"] <- verif.latitude(fixed.dataframe[,"latitude"]) # fonction qui vérifie si les valeurs de latitude sont valides (+-90), sinon remplace par un NA
+  
+  verif.longitude <- function(x, max = 180, min = -180){
+    replace(x, x > max | x < min, NA)
+  }
+  fixed.dataframe[,"longitude"] <- verif.longitude(fixed.dataframe[,"longitude"]) # fonction qui vérifie si les valeurs de longitude sont valides (+-180), sinon remplace par un NA
+  
+  fixed.dataframe
 }

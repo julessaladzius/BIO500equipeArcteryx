391d3bdb5917683286ac23ea9c3c69c5cbedc017
fm4rtin <marf3212@usherbrooke.ca>
2025-04-19 20:30:02 -0400

    1er essai de target

--- _targets.R
+++ _targets.R
@@ -1,1 +1,13 @@
-essai
+setwd("/Users/francoismartin/Desktop/PROJETBIO500/BIO500equipeArcteryx")
+library("targets")
+
+tar_option_set(
+  packages = c("dplyr", "readr","ggplot2","tidyverse")
+)
+
+list(
+  tar_target(
+    donnees,
+    read.csv("data/raw/donnees.csv")
+  )
+)

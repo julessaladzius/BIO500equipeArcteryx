67c603e328e834b42332880c4f644f12c23530c0
fm4rtin <marf3212@usherbrooke.ca>
2025-04-20 17:40:12 -0400

    modif fct_integration

--- Scripts/Fonctions_depannage_debug_target.R
+++ Scripts/Fonctions_depannage_debug_target.R
@@ -88,9 +88,8 @@ Keys_mold<-function(donnees){
   
   #--------INTÉGRER TAXO À DONNEES--------
   
-  fct_integration <- function(Donnees_filtrees,Tab_taxo){
-  
-  donnees <- merge(Tab_taxo,Donnees_filtrees,by="observed_scientific_name")
+  fct_integration <- function(Tab_taxo){
+  donnees <- merge(taxo,donnees,by="observed_scientific_name")
   }
   
   #--------TABLEAU PRIMAIRE POPULATION--------
